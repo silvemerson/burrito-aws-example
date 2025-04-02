9730886d1c99ee3a0955b04f1ad9f401a51ce1fd
Emerson Silva <39895958+silvemerson@users.noreply.github.com>
2025-04-02 17:06:56 -0300

    Create main.tf

---
+++ s3-example/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1" # Escolha a região da AWS
+}
+
+resource "aws_s3_bucket" "hello_bucket" {
+  bucket = "meu-hello-world-bucket-123456" #Escolha um nome único
+}
+
+resource "aws_s3_object" "hello_file" {
+  bucket  = aws_s3_bucket.hello_bucket.bucket
+  key     = "hello-world.txt"
+  content = "Hello, World from Terraform!"
+}
